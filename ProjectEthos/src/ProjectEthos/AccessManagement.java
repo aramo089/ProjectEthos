1240efa6ecf507d576d74d757fb1f17518a501b0
Philip Dacosta <pdaco001@fiu.edu>
2020-04-20 23:26:22 -0400

    This commit completes the AccessManagement class.

--- ProjectEthos/src/ProjectEthos/AccessManagement.java
+++ ProjectEthos/src/ProjectEthos/AccessManagement.java
@@ -1,32 +1,66 @@
 package ProjectEthos;
+import java.util.Date;
+
+/**
+ * 
+ * This class will handle the Access Management and essentially act as a controller.
+ * This will hand the addition/removal/assignment of users & groups
+ * This will hand the addition/removal of system access
+ * 
+ * @Group1
+ */
+
 
 public class AccessManagement {
 
-	private int userID;
-	private int eventId;
-	private String eventDescription;
+	private int userID; //This attribute will hold the user object
+	private Date eventdate; //This will hold the date value of when an event occurs
+	private String eventDescription; //This will contain a description of the event.
 	
+	/**
+	 * This operator will use the userID attribute and add the user to the AccessManagment
+	 */
 	public void addUser() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and remove the user to the AccessManagment
+	 */
 	public void removeUser() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and add groups from a user in AccessManagment
+	 */
 	public void assignUserstoGoup() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and remove groups from a user in AccessManagment
+	 */
 	public void removeUserstoGroup() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and add system access for a user in AccessManagment
+	 */
 	public void addSystemAccess() {
 	}
-	
+	/**
+	 * This operator will use the userID attribute and remove system access for a user in AccessManagment
+	 */
 	public void removeSystemAccess() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and add groups for users to be assigned in AccessManagment
+	 */
 	public void addGroups() {
 	}
 	
+	/**
+	 * This operator will use the userID attribute and remove groups for users that were assigned in AccessManagment
+	 */
 	public void removeGroups() {
 	}
 	
