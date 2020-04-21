3a063623b300601453151971c920d6801c664b46
Philip Dacosta <pdaco001@fiu.edu>
2020-04-20 23:36:23 -0400

    This commit contains additional comments and documentation for the classes. Alarm and Door.

--- ProjectEthos/src/ProjectEthos/Alarm.java
+++ ProjectEthos/src/ProjectEthos/Alarm.java
@@ -2,11 +2,24 @@ package ProjectEthos;
 
 import java.util.Date;
 
+/**
+ * 
+ * This class is an alarm object that will be used as part of the schedule management class.
+ * This class will notify owners that are assigned to the alarm object.
+ * 
+ * @Group1
+ */
+
+
 public class Alarm {
 
-	private String alarmName;
-	private Date alarDateTime;
-
+	private String alarmName; //This attribute will define the Alarm Name that will be used alert owners
+	private Date alarmTime; // This attribute will define when the Alarm will go off.
+/*
+ * This will notify the owners when the notifyOwners operator is called. 
+ * This alarm object is used in the schedule management class to create schedules.
+ * Each alarm is unique and must be an object.
+ */
 	public void notifyOwners() {
 	}
 	
--- ProjectEthos/src/ProjectEthos/Door.java
+++ ProjectEthos/src/ProjectEthos/Door.java
@@ -1,13 +1,28 @@
 package ProjectEthos;
 
+/**
+ * 
+ * This class is an door object that will be used as part of the panel switch class.
+ * This class is representative of a door object and will be used for security type doors.
+ * 
+ * @Group1
+ */
 public class Door {
 	
-	private String door;
-	private String doorLocation;
-	
+	private String door; //This attribute will be named after a door that is located in the building.
+	private String doorLocation; // This attribute will be used locate where this object belongs in the building.
+	/*
+	 * The swithOn operator will be called when passes validation. 
+	 * This will open the door for the person that needs to pass through the door.
+	 */
 	public void switchOn() {
 	}
 	
+	/*
+	 * This switchOff operator occurs when the person has passed through the door. 
+	 * This will cause the door to lock behind them. 
+	 * This prevents an unauthorized personnel to enter.
+	 */
 	public void switchOff() {
 	}
 	
