8565fe72f72958e4e08c761bc9463c32972e525e
aramo089 <61912284+aramo089@users.noreply.github.com>
2020-04-22 00:03:07 -0400

    Added ExitButton

---
+++ ProjectEthos/src/ProjectEthos/exitButton.java
@@ -0,0 +1,20 @@
+package ProjectEthos;
+
+/**
+ * 
+ * This class will allow a door object to be opened manually.
+ *
+ */
+
+public class exitButton {
+
+	private boolean isClosed; //This attribute will hold the Boolean value if the door is closed or open
+
+	/**
+	 * This operator that signal a door object to open. 
+	 */
+	
+	public void pressToOpen() {
+	}
+	
+}
