3f85875b5c915e7d1f539a03148ab710352817f4
Philip Dacosta <pdaco001@fiu.edu>
2020-04-21 23:39:15 -0400

    This contains an update to the description for the EventLog.

--- ProjectEthos/src/ProjectEthos/EventLog.java
+++ ProjectEthos/src/ProjectEthos/EventLog.java
@@ -1,15 +1,33 @@
 package ProjectEthos;
 
+import java.util.ArrayList;
+
+
+/**
+ * 
+ * @author Group1
+ *	This object will contain the Event Log information for all the changes occurring in the system.
+ *  This will also contain the log for any event that may occur such as adding a user, removing a user, 
+ *  adding groups, removing groups, employees accessing the building. This will be the central source of events
+ *  which will then be used for the reporting in the application.
+ *  
+ */
+
 public class EventLog {
 	
-	private int eventID;
-	private String systemLog;
+	private ArrayList  eventLog;  //This will contain the event log information
+	private ArrayList  systemLog; //This will contain the system log information
 	
+	/**
+	 * This operator will record the log information into the event or system log.
+	 */
 	public void recordLog() {
 	}
 	
+	/**
+	 * This operator will reset the log information for the event log or system log.
+	 */
 	public void resetLog() {
 	}
 	
-
 }
